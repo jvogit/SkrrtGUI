2214ef687812ab40e76fd7934d471dcc1443c1aa
jvogit <mcraftworldmc@gmail.com>
2018-10-16 16:48:46 -0700

    Fix focus mode GUI add toggle switching

--- FocusModeGUI.py
+++ FocusModeGUI.py
@@ -10,10 +10,22 @@ class FocusModeGUI(ttk.Frame):
         self.create_widgets()
         self.grid_widgets()
         
+    #width 94 height 31 !
     def create_widgets(self):
-        self.statusBar = Label(self, width=94, height=5, relief='groove', textvariable=self.app.statusVar)
-        self.speedometer = Label(self, width = 30, height=26, relief='groove', textvariable=self.app.speedVar)
+        self.statusBar = Label(self, width=90, height=5, relief='groove', textvariable=self.app.statusVar)
+        self.revert = Button(self, width=30, height=8, relief='flat', text='GEAR', command=self.app.switchFrame)
+        self.speed = Label(self, width=30, height=25, textvariable=self.app.speedVar)
+        self.placeholder1 = Button(self, width=30, height=8, relief='flat', state=DISABLED)
+        self.placeholder2 = Button(self, width=30, height=8, relief='flat', state=DISABLED)
+        self.placeholder3 = Button(self, width=30, height=8, relief='flat', state=DISABLED)
+        
     def grid_widgets(self):
-        self.statusBar.grid(column=0, row = 0, sticky='NEWS')
-        self.speedometer.grid(column=0, row=1, sticky='NEWS')
+        self.statusBar.grid(column=0, row = 0, columnspan=3, sticky='news')
+        self.placeholder1.grid(column=0, row=1, sticky='news')
+        self.placeholder2.grid(column=1, row=2, sticky='news')
+        self.placeholder3.grid(column=2, row=3, sticky='news')
+        self.revert.grid(column=0, row=1, rowspan=3, sticky='news')
+        self.revert.tkraise()
+        self.speed.grid(column=1, row=1, rowspan=3, sticky='news')
+        self.speed.tkraise()
         pass
